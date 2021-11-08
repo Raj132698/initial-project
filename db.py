199bfeecaa36fbeaff7a82cf0f3a96c61c370c84
Raj <rajdhn10000@gmail.com>
2021-11-08 14:12:35 +0530

    making changes in first branch

---
+++ db.py
@@ -0,0 +1,65 @@
+
+
+import boto3
+from boto3.dynamodb.conditions import Attr, Key
+from botocore.exceptions import ClientError
+
+
+
+
+
+
+
+
+
+PRIMARY_KEY     = "email_id"
+NAME            = "name"
+REMAINING_LEAVE = "remaining_leave"
+REGION          = "region"
+LEAVE_TYPE      = "leave_type"
+LEAVE_DURATION  = "leave_duration"
+DATES_APPLIED   = "dates_applied"
+LEAVE_STATUS    = "leave_status"
+STATUS          = "status"
+
+DATA_ATTRIBUTE = "LEAVE-PORTAL"
+
+DB=boto3.resource('dynamodb')
+table= DB.Table(DATA_ATTRIBUTE)
+
+def put_leave():
+    # Mainly to put the value after selecting in dynamodb
+
+    items = {
+        PRIMARY_KEY     :    f"{email_id}",
+        NAME            :    f"{name}",
+        REMAINING_LEAVE :    f"{remaining_leave}",
+        REGION          :    f"{region}",
+        LEAVE_TYPE      :    f"{leave_type}",
+        LEAVE_DURATION  :    f"{leave_duration}",
+        DATES_APPLIED   :    f"{dates_applied}",
+        LEAVE_STATUS    :    f"{leave_status}",
+        STATUS          :    f"{status}"
+    }
+
+    kwargs = {"Item": items, "ConditionExpression": Attr(PRIMARY_KEY).not_exists()}
+    table.put_item(**kwargs)
+    print("Items has been inserted")
+
+
+def get_leave_details():
+    primary_key_val=f"{email_id}"
+    try:
+        response = table.get_item(Key={PRIMARY_KEY: primary_key_val})
+
+
+
+
+def delete_marked_leave():
+
+
+    primary_key_val = f"{email_id}"
+    kwargs = {"Key": {PRIMARY_KEY: primary_key_val}}
+    table.delete_item(**kwargs)
+    print("Item deleted")
+    
